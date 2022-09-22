369753b13139ae7bd8692e2f355a2f5f6bf93471
Seokseong Jeon <sjeon87+github@gmail.com>
2022-09-22 14:31:02 +0900

    add compiled files

--- dist/cjs/index.d.ts
+++ dist/cjs/index.d.ts
@@ -1,1 +1,2 @@
+export { buildTable } from "./tableBuilder";
 export { parse as parsePcap, parseEutraFeatureSets, parseMrdcFeatureSetCombinations, parseNrFeatureSets, parseMrdcSupportedBandCombinationList, } from "./pcap";
---
+++ dist/cjs/tableBuilder.d.ts
@@ -0,0 +1,5 @@
+export declare function buildTable(objList: Object[], lines: (string | number)[][], { title, indexName, indexOffset, }: {
+    title: string;
+    indexName: string;
+    indexOffset?: number;
+}): void;
